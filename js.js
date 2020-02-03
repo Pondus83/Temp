b75892fd5b37d1e5bb8fae3c2f6717472555a79a
Pondus83 <55687200+Pondus83@users.noreply.github.com>
2020-02-03 13:55:50 +0100

    Viker

---
+++ js.js
@@ -0,0 +1,11 @@
+var temp = 20; // Skal være inden for -20 til 30
+var varmeFelt = document.querySelectorAll("#varme .st1");
+var kuldeFelt = document.querySelectorAll("#kulde .st0");
+
+if (temp >= 0) {
+    for (i = 0; i < temp; i++) {
+        varmeFelt[i].classList.add(this.add
+            
+            // getAttribute("data-add"));
+    }
+}
